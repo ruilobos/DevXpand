6ded65118eed63bd74247b4b1746d945318b6028
ruilobos <ruilobos@gmail.com>
2020-08-13 11:37:58 +0100

    fixing static files

--- devxpand/devxpand/settings/base.py
+++ devxpand/devxpand/settings/base.py
@@ -133,7 +133,7 @@ MEDIA_URL = '/media/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'core.static'),
 )
 
 # E-mails
